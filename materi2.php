10d76c60ac66a9fdc9545fcf0cf5baddb216d28a
khudziawanmuhammadyasir-web <muhammadyasirr1712@gmail.com>
2026-04-20 08:46:50 +0800

    update materi 2

---
+++ materi2.php
@@ -0,0 +1,16 @@
+<form method="POST">
+    Masukkan Sebuah Angka : <input type = "number" name = "angka">
+    <input type="submit" value="kirim">
+</form>
+
+<?php
+
+if(isset($_POST['angka'])) {
+    $angka = $_POST['angka'];
+
+    for ($i = 1; $i <= $angka; $i++) {
+        echo "<br>Nilai Anda : $i";
+    }
+}
+
+?>
