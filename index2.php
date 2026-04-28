78b1150b4e684ca842bc3fd4adc99386046ed82a
khudziawanmuhammadyasir-web <muhammadyasirr1712@gmail.com>
2026-04-28 21:15:31 +0800

    update materi

--- index2.php
+++ index2.php
@@ -3,8 +3,8 @@
 echo '<br><br><a href="materi1.php"><button>Ke Materi 1 : If dan Else</button></a>';
 echo '<br><br><a href="materi2.php"><button>Ke Materi 2 : Looping</button></a>';
 echo '<br><br><a href="materi3.php"><button>Ke Materi 3 : Function</button></a>';
-echo '<br><br><a href="materi1.php"><button>Ke Materi 2 : Looping</button></a>';
-echo '<br><br><a href="materi1.php"><button>Ke Materi 2 : Looping</button></a>';
+echo '<br><br><a href="materi3lnjutan.php"><button>Ke Materi 3 : Function Lanjutan</button></a>';
+echo '<br><br><a href="materi3lnjutan2.php"><button>Ke Materi 3 : Function Lanjutan1</button></a>';
 echo '<br><br><a href="materi1.php"><button>Ke Materi 2 : Looping</button></a>';
 echo '<br><br><a href="materi1.php"><button>Ke Materi 2 : Looping</button></a>';
 echo '<br><br><a href="materi1.php"><button>Ke Materi 2 : Looping</button></a>';
